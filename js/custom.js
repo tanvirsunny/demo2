6a62dc9c1cb7de3a005ee5b509ea088d108df4a4
Tanvir Sunny <32938191+tanvirsunny@users.noreply.github.com>
2018-07-25 00:30:13 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
           $(this).toggleClass('active');
     });
 
+    $(window).scroll(function(){
+      if($(document).scrollTop()>50){
+
+          $('.navbar').addClass('navnew');
+      }
+      else{
+          $('.navbar').removeClass('navnew');
+      }
+        
+    });
+
 
 
     $('.one-time').slick({
@@ -15,129 +26,21 @@ $(document).ready(function(){
       dots: true,
       infinite: true,
       speed: 1000,
-      slidesToShow: 1
-    });
-
-
-     $('#fullpage').fullpage({
-        anchors:['header', 'services', 'protfulio-area', 'our-team','contact-us'],
-        verticalCentered: true,
-        scrollOverflow: true,
-        scrollingSpeed: 1500
-    });
-
-     particlesJS("particles-js", {
-      "particles": {
-        "number": {
-          "value": 380,
-          "density": {
-            "enable": true,
-            "value_area": 800
-          }
-        },
-        "color": {
-          "value": "#d5ddf1"
-        },
-        "shape": {
-          "type": "circle",
-          "stroke": {
-            "width": 0,
-            "color": "#000000"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-          "image": {
-            "src": "img/github.svg",
-            "width": 0,
-            "height": 0
-          }
-        },
-        "opacity": {
-          "value": 0.5,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 4,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
-          "enable": false,
-          "distance": 150,
-          "color": "#ffffff",
-          "opacity": 0.4,
-          "width": 1
-        },
-        "move": {
-          "enable": true,
-          "speed": 1,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
-        }
-      },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": false,
-            "mode": "grab"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 140,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 4
-          },
-          "remove": {
-            "particles_nb": 2
-          }
-        }
-      },
-      "retina_detect": true
+      slidesToShow: 3
     });
 
 })
+
+    var tl = new TimelineLite();
+    var $heading1 = $('.heading-1');
+    var $heading2 = $('.heading-2');
+    var $buttonarea = $('.hire');
+    var $imageanim = $('.home-img');
+
+    tl.to($heading1, 1.4, {x:0,y:0, autoAlpha: 1});
+    tl.to( $heading2, 1.4, {x:0,y:0, autoAlpha: 1});
+    tl.to( $buttonarea, 1.3, {x:0,y:0, autoAlpha: 1});
+    tl.to( $imageanim, 1.5, {x:0,y:0, autoAlpha: 1});
 
     var controller=new ScrollMagic.Controller();
 
