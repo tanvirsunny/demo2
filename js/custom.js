a156b1c681a93dcfb191520ab06078ec4bc9f386
Tanvir Sunny <32938191+tanvirsunny@users.noreply.github.com>
2018-08-30 01:20:15 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -10,28 +10,34 @@ $(document).ready(function(){
     }).slickAnimation();
 
 
-    $('.multiple-items').slick({
-      autoplay: true,
-      slidesToShow: 4,
-      slidesToScroll: 1
-    });
-
-     $('.visa').slick({
-      autoplay: true,
-      slidesToShow: 2,
-      slidesToScroll: 1
-    });
-     $('.testimonials').slick({
-      autoplay: true,
+ $('.album-slider').slick({
       slidesToShow: 1,
-      slidesToScroll: 1
-    });
+      slidesToScroll: 1,
+      speed:1000
+    }).slickAnimation();
 
 
- // $('.slider_wrap').slick({
- //      slidesToShow: 1,
- //      slidesToScroll: 1,
- //      speed:1000
- //    }).slickAnimation();
+
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
+// filter functions
+
+// bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+   
     
 })
